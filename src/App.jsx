b5d75c330f6660a072ae5fdc0518fe768040ae73
zhangwen.chen <623025156@qq.com>
2024-03-28 19:09:27 +0800

    feat: link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, Link } from 'react-router-dom';
 import routes from './routes';
 import './App.css';
 
 function App() {
   const views = useRoutes(routes);
-  return views;
+  return (
+    <>
+      {routes.map((route, index) => (<Link key={route.path} to={route.path}>{index+1}. {route.name}</Link>))}
+      {views}
+    </>
+  );
 }
 
 export default App;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,12 @@
-import Dashboard from '../views/Dashboard.jsx';
 import Counter from '../views/Counter.jsx';
 
 const routes = [
-  {
-    path: '/',
-    element: <Dashboard />,
-  },
-  { path: '/counter', element: <Counter /> },
+  // {
+  //   path: '/',
+  //   element: <Dashboard />,
+  //   name: 'Dashboard',
+  // },
+  { path: '/counter', element: <Counter />, name: 'Counter' },
 ];
 
 export default routes;
