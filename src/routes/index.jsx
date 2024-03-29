bb5944fe5dac3472fb52ee0bd37500b5f3af3df5
zhangwen.chen <623025156@qq.com>
2024-03-29 14:37:22 +0800

    feat: add useTimeout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,9 @@
 import Counter from '../views/Counter.jsx';
+import Timeout from '../views/Timeout.jsx';
 
 const routes = [
-  // {
-  //   path: '/',
-  //   element: <Dashboard />,
-  //   name: 'Dashboard',
-  // },
   { path: '/counter', element: <Counter />, name: 'Counter' },
+  { path: '/useTimeout', element: <Timeout />, name: 'UseTimeout' },
 ];
 
 export default routes;
---
+++ src/views/Timeout.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from "react";
+
+function useTimeout(callback, deley) {
+  const callbackRef = useRef(callback);
+  callbackRef.current = callback;
+  useEffect(() => {
+    // callbackRef.current is to keep callback up to date
+    const handler = setTimeout(() => callbackRef.current(), deley);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [deley]);
+
+  return callbackRef.current;
+}
+
+// test
+export default function Timeout() {
+	const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setValue(8);
+  }, []);
+
+  useTimeout(() => {
+    // if callbackRef.current is callback，log data is 0
+    console.log(value);
+  }, 1000);
+  useTimeout(() => {
+    console.log(2);
+  }, 1000);
+  useTimeout(() => {
+    console.log(3);
+  }, 100);
+  useTimeout(() => {
+    // log 3 8 2 4
+    console.log(4);
+  }, 2000);
+
+  return (
+    <div className="App">
+      <p>Value: {value}</p>
+      <button onClick={() => setValue(value + 1)}>Click Me</button>
+    </div>
+  );
+  }
