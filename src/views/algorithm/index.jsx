52ff56e92c0488b8f7d9440afe404e6ea24d9a01
zhangwen.chen <623025156@qq.com>
2024-04-16 23:55:18 +0800

    feat: group

--- src/views/algorithm/index.jsx
+++ src/views/algorithm/index.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react';
 import './index.css';
-// import reactSvg from '../../assets/react.svg'
-// const imgArr = ['合并有序数组'];
 const files = import.meta.glob('../../assets/**/*.png', {eager: true});
-const imgArr = Object.keys(files).map(fileName => {
+let imgArr = Object.keys(files).map(fileName => {
   let fileType = fileName.match(/([^/]*?)\.[^/.]+$/)[1] //用正则匹配出文件名称
   return {
     name: fileType, 
     src: files[fileName].default, 
   }
 });
-// const ticks = imgArr.map(item => ({name: item, src: import.meta.globEager(`../../assets/${item}.png`)}))
+const group = ['string', 'array', 'two-points', 'window', 'matrix', 'map', 'intervals', 'stack', 'link', 'binary', 'graph', 'dp']
 function Algorithm() {
-  // const [count, setCount] = useState(0);
-  console.log("🚀 ~ imgArr ~ imgArr:", imgArr)
-
+  const [imgs, setImgs] = useState(imgArr);
+  function changeGroup(value) {
+    const arr = imgArr.filter(item=> item.name.includes(value))
+    setImgs(arr);
+  }
   return (
     <>
+      {group.map(item => (
+        <button key={item} onClick={() => changeGroup(item)}>{item}</button>
+      ))}
       <div className="img-container">
-        {imgArr.map((item) => (
+        {imgs.map((item) => (
           <div className='img-box' key={item.name}>
             <h3 >{item.name}</h3>
             <img  src={item.src} alt={item.name} />
